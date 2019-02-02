675c078521e89af76359a5a772687359802be744
mattperkins <github@mattperkins.com>
2019-02-02 16:10:06 +0000

    add schema resolvers and working logic

--- app.js
+++ app.js
@@ -18,19 +18,28 @@ app.use('/graphql', graphqlHttp({
     type RootQuery {
       events: [String!]!
     }
-
     type RootMutation {
       createEvent(name: String): String
     }
+
     schema {
       query: RootQuery
       mutation: RootMutation
     }
   `),
-  // resolver functions that match schema endpoints
+  // schema logic 'resolvers' = functions that match schema endpoints 
   rootValue: {
-
-  }
+    // 'events' resolver corresponding to the 'events' RootQuery 
+    events: () => {
+      return ['C8691', 'NBr252', 'Le9175']
+    },
+    // 'createEvent' resolver corresponding to the 'createEvent' RootMutation (which accepts arguments (args) === name: String)
+    createEvent: (args) => {
+      const eventName = args.name
+      return eventName
+    }
+  },
+  graphiql: true
 }))
 
 const PORT = 3000
