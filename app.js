dc626b22c98ecb1d209fb975d2184e7da406c9c5
mattperkins <github@mattperkins.com>
2019-02-02 15:13:46 +0000

    configure endpoint route

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ const app = express()
 
 app.use(bodyParser.json())
 
+app.get('/', (req, res, next) => {
+  res.send('Endpoint reached!')
+})
+
 const PORT = 3000
-app.listen(PORT, console.log(`Server running on $PORT`))
+app.listen(PORT, console.log(`Server running on ${PORT}`))
