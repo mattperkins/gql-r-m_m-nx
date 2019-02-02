55ad55b47cf2360515f00ba1998a7fb7238963a9
mattperkins <github@mattperkins.com>
2019-02-02 15:45:50 +0000

    add graphql route

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const graphqlHttp = require("express-graphql")
+const { buildSchema } = require("graphql")
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-  res.send('Endpoint reached!')
-})
+// app.get('/', (req, res, next) => {
+//   res.send('Endpoint reached!')
+// })
+
+// middleware to parse schema queries and resolvers
+app.use('/graphql', graphqlHttp({
+  // configure graphql api
+  schema: null,
+  // resolver functions that match schema endpoints
+  rootValue: {
+
+  }
+}))
 
 const PORT = 3000
 app.listen(PORT, console.log(`Server running on ${PORT}`))
